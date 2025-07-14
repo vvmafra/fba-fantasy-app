11268de1484d9a1f5fd81a46cc8f413fb9f0fe95
vvmafra <victorvmafra97@gmail.com>
2025-07-14 01:00:13 -0300

    console log errors api

--- apps/frontend/src/lib/api.ts
+++ apps/frontend/src/lib/api.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
-// import { config } from './config';
+import { config } from './config';
+
+console.log("oi")
+console.log('API URL em uso:', config.apiUrl);
 
 // Configuração base do cliente HTTP
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: config.apiUrl,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
